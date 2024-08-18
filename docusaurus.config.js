3982a1d03353c0f48cb85fa6cbc5099ef7879673
govolokatliai <govolokatliai@gmail.com>
2024-08-19 02:56:29 +0800

    增加了静态资源的下载方式

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  staticDirectories: ['static'],
+  //staticDirectories: ['static'],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
